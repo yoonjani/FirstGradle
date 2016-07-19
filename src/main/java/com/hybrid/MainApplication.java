42ad3d74c4a20570cf7c8e98ec4a98ea18c5f346
java <java>
2016-07-19 17:07:02 +0900

    first commit

---
+++ src/main/java/com/hybrid/MainApplication.java
@@ -0,0 +1,11 @@
+package com.hybrid;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("###################");
+		System.out.println("Hello First Gradle");
+		System.out.println("###################");
+	}
+
+}
